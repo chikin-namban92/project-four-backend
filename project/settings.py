e81f787822312e3dccaede0040aa6fcf97a43142
James Guppy <j.guppy321@gmail.com>
2024-03-21 21:10:47 +0000

    restored default database url in settings.py

--- project/settings.py
+++ project/settings.py
@@ -103,7 +103,9 @@ if str(os.getenv('ENVIRONMENT')) == 'development':
         'PORT': 5432
     }
 else:
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+    DATABASES['default'] = dj_database_url.config(
+        default='postgres://...',conn_max_age=600, ssl_require=True
+        )
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
